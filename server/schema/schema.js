09a13610285a37ec9e7c0067c6efc444111abcac
Corneliu <corneliu.gaina@gmail.com>
2019-07-10 15:45:26 +0200

    step 4 ok

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,22 +1,23 @@
 const graphql = require('graphql'); // 1. on va attribuer à une variable le paquet npm graphql précédemment installé
+const _ = require('lodash'); // 7. apres avoir installé lodash 
+
 
 const { GraphQLObjectType, 
         GraphQLString, 
         GraphQLSchema,
+        GraphQLInt, // cf. date 
         GraphQLID,
         GraphQLList } = graphql; // 2. ici on va extraire les objets dont on a besoin du package graphql 
 
-
 // 3. dummy data 
 const octocats = [
-    {id: 22,prenom: "Cedric",nom: "Van Hove",gitHub: "cevaho",linkedIn: "https://www.linkedin.com/in/c%C3%A9dric-van-hove-99250926/",promo: "johnson"},
-    {id: 1, prenom: "Maxime", nom: "Broodcoorens",gitHub: "Broodco", linkedIn: "https://www.linkedin.com/in/maxime-broodcoorens-783472168/", promo: "lovelace" },
-    {id: 2, prenom: "Emilie", nom: "Bialais",gitHub: "ebialais",linkedIn:"https://www.linkedin.com/in/emilie-bialais-b4b67658/", promo: "lovelace" },
-    {id: 3,prenom: "Pierre-Louis ",nom: "Picard",gitHub: "Pierre-Louis242",linkedIn:"https://www.linkedin.com/in/pierre-louis-picard-b30993a8/",promo: "lovelace"},
-    {id: 4,prenom: "Mathilde",nom: "Baquet",gitHub: "MathildeBa",linkedIn:"https://www.linkedin.com/in/mathilde-baquet",promo: "lovelace"},
-    {id: 19,prenom: "Antoine",nom: "Diambu",gitHub: "AntoineDia",linkedIn: "https://www.linkedin.com/in/antoine-diambu-402776178/",promo: "johnson" }
+    {id: "22", prenom: "Cedric",nom: "Van Hove",gitHub: "cevaho",linkedIn: "https://www.linkedin.com/in/c%C3%A9dric-van-hove-99250926/",promo: "johnson"},
+    {id: "1", prenom: "Maxime", nom: "Broodcoorens",gitHub: "Broodco", linkedIn: "https://www.linkedin.com/in/maxime-broodcoorens-783472168/", promo: "lovelace" },
+    {id: "2", prenom: "Emilie", nom: "Bialais",gitHub: "ebialais",linkedIn:"https://www.linkedin.com/in/emilie-bialais-b4b67658/", promo: "lovelace" },
+    {id: "3", prenom: "Pierre-Louis", nom: "Picard",gitHub: "Pierre-Louis242",linkedIn:"https://www.linkedin.com/in/pierre-louis-picard-b30993a8/",promo: "lovelace"},
+    {id: "4", prenom: "Mathilde",nom: "Baquet",gitHub: "MathildeBa",linkedIn:"https://www.linkedin.com/in/mathilde-baquet",promo: "lovelace"},
+    {id: "19", prenom: "Antoine",nom: "Diambu",gitHub: "AntoineDia",linkedIn: "https://www.linkedin.com/in/antoine-diambu-402776178/",promo: "johnson" }
 ]
-
 
 
 // 4. on va créer deja notre 1er type dans le schema (UserType), on donne un nom à notre objet (octocat) + fields avec le type correspondant
@@ -32,3 +33,21 @@ const UserType = new GraphQLObjectType ({
     })
 }); //!\ 
 
+
+const RootQuery = new GraphQLObjectType({ // 5. represents all of the possible entry points into the GraphQL API
+    name: 'RootQueryType', // on specifie le type 
+    fields: { 
+        octocat: {
+            type: UserType,
+            args: {id: {type: GraphQLID}}, //!\ si notre ID est un string, il mettre un integer (GraphQLInt) et parser
+            resolve(parent, args){ // 8. lorsqu'on fera une query (dans GraphiQL), c'est la fonction resolve qui se lance
+                return _.find(octocats, {id: args.id});
+            }
+        }
+    }
+})
+
+
+module.exports = new GraphQLSchema({ // 6. ici on va exporter notre schèma 
+    query: RootQuery,    
+})
